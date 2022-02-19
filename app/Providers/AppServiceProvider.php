4c727827ba44a32ed30229f2a76a860b0dafd72b
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-02-19 21:28:27 +0700

    Define Admin and Receptionist Gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Paginator using Bootstrap
+        Paginator::useBootstrap();
+
+        // Define Admin and Receptionist Gate
+        Gate::define('admin', function(User $user) {
+            return $user->role === 'administrator';
+        });
+
+        Gate::define('receptionist', function(User $user) {
+            return $user->role === 'receptionist';
+        });
     }
 }
