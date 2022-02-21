4d09fba06ec571203b94a6ec0c285d9b4a4c9255
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-02-21 21:05:41 +0700

    Dashboard Admin (User CRUD)

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.admin')
+
+@section('container')
+    
+<h1 class="h2">Create User</h1>
+
+<div class="col-lg-8">
+    <a href="{{ route('user.index') }}" class="btn btn-secondary mb-3">Back</a>
+
+    <form action="{{ route('user.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Nama</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" required autofocus value="{{ old('name') }}" placeholder="Nama">
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="username" class="form-label">Username</label>
+            <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username" required autofocus value="{{ old('username') }}" placeholder="Username">
+            @error('username')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required autofocus placeholder="Password">
+            @error('password')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="username" class="form-label">Role</label>
+            <select name="role" id="role" class="form-select">
+                <option value="hotel_guest">Hotel Guest</option>
+                <option value="administrator">Administrator</option>
+                <option value="receptionist">Receptionist</option>
+            </select>
+            @error('username')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+@endsection
