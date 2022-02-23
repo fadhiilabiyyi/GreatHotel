8630972a829ada58b52e4c7e2fa15e50b3bd34df
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-02-23 20:56:59 +0700

    Fix Room Facilities

--- resources/views/admin/room-facilities/create.blade.php
+++ resources/views/admin/room-facilities/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     
-<h1 class="h2">Create Fasilitas Hotel</h1>
+<h1 class="h2">Create Fasilitas Kamar</h1>
 
 <div class="col-lg-8">
     <a href="{{ route('room-facilities.index') }}" class="btn btn-secondary mb-3">Back</a>
